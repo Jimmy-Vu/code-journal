9950ea9cc861ab546bc306862bf0b2d733a6eb28
Jimmy Vu <JMV3211@gmail.com>
2021-09-08 11:32:12 -0700

    Fix all error #1

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ var photo = document.querySelector('#main-image');
 
 function updatePhoto(event) {
   photo.setAttribute('src', event.target.value);
+  if (event.target.value === '') {
+    photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+  }
 }
 
 urlInput.addEventListener('input', updatePhoto);
