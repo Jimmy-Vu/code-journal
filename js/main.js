9799ed94a5190ec9cfd46fdfa5fa4110b0805284
Jimmy Vu <JMV3211@gmail.com>
2021-09-08 17:13:43 -0700

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML. #2

--- js/main.js
+++ js/main.js
@@ -32,3 +32,40 @@ function submitListener(event) {
 }
 
 form.addEventListener('submit', submitListener);
+
+var entryUL = document.querySelector('#entry-list');
+
+function entryAdd(entry) {
+  var entryListing = document.createElement('li');
+  var firstChild = entryUL.firstChild;
+  entryListing.className = 'row entry';
+  entryUL.insertBefore(entryListing, firstChild);
+
+  var entryImage = document.createElement('img');
+  entryImage.className = 'column-half';
+  entryImage.setAttribute('src', entry.photoURL);
+  entryListing.appendChild(entryImage);
+
+  var entryDiv = document.createElement('div');
+  entryDiv.className = 'column-half';
+  entryListing.appendChild(entryDiv);
+
+  var entryTitle = document.createElement('h2');
+  var entryTitleText = document.createTextNode(entry.title);
+  entryTitle.appendChild(entryTitleText);
+  entryDiv.appendChild(entryTitle);
+
+  var entryNotes = document.createElement('p');
+  var entryNotesText = document.createTextNode(entry.notes);
+  entryNotes.appendChild(entryNotesText);
+  entryDiv.appendChild(entryNotes);
+}
+
+var testObject =
+    {
+      title: 'Jimmy',
+      photoURL: 'https://camo.githubusercontent.com/eaf1d896ea6dc83f571a87ce0fa5f99e8fedd4dc8e217a855c153e065b3c3ad5/68747470733a2f2f6c667a2d7374617469632e73332d75732d776573742d312e616d617a6f6e6177732e636f6d2f67726170686963732f776972656672616d65732f636f64652d6a6f75726e616c2f6669676d612f757365722d63616e2d766965772d74686569722d656e74726965732d6d6f62696c652e706e67',
+      notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    };
+
+entryAdd(testObject);
